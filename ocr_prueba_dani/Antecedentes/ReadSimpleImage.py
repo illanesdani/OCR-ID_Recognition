f90b3dd2e8f0a85baf1cdd7a701d72a6e5c10b8c
Daniela Illanes <illanesdaniela28@gmail.com>
2024-10-14 11:40:13 -0300

    parte 1, imagenes y procesamiento base

---
+++ ocr_prueba_dani/Antecedentes/ReadSimpleImage.py
@@ -0,0 +1,29 @@
+import pytesseract
+import cv2
+import json
+from PIL import Image
+import re
+
+#el codigo a continuación lee sólo una imagen normal y simple
+
+#configuración de los numeros a utilizar para el psm y el oem
+myconfig= r"--psm 6 --oem 3"
+
+#Especificacion de la ruta de tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+try:
+    image_path="text_example.jpg"
+    img= cv2.imread(image_path)
+    text = pytesseract.image_to_string(img, config=myconfig)
+    heoght, width, _ = img.shape
+    print(text)
+
+except FileNotFoundError:
+    print(f"Error: El archivo {image_path} no se encuentra.")
+except pytesseract.pytesseract.TesseractError as e:
+    print(f"Error al ejecutar Tesseract: {e}")
+except Exception as e:
+    print(f"Error inesperado: {e}")
+            
