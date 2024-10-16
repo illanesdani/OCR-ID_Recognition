a60620a04e0f31445f986cada86890067778684c
Daniela Illanes <illanesdaniela28@gmail.com>
2024-10-16 10:56:08 -0300

    actualization of venv

---
+++ ocr_id_recognition/Main.py
@@ -0,0 +1,213 @@
+import pytesseract
+import cv2
+import json
+from PIL import Image
+import re
+import random
+from pytesseract import Output
+import numpy as np
+import pytesseract.pytesseract
+
+
+#configuracion de la orientacion de la pagina y el modelo de ocr
+custom_config = r"--psm 11 --oem 3"
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+# **
+# Selecciona una imagen de las existentes aleatoriamente para analizar
+# **
+def random_image():
+    num = random.randint(1, 10)
+    rutas = {
+        1: "DocumentosPrueba/dni_argentina_nuevo.png",
+        2: "DocumentosPrueba/dni_argentina.jpg",
+        3: "DocumentosPrueba/dni_brasil.jpg",
+        4: "DocumentosPrueba/dni_chile.png",
+        5: "DocumentosPrueba/dni_espana.jpg",
+        6: "DocumentosPrueba/dni_newYork.jpg",
+        7: "DocumentosPrueba/dni_peru.png",
+        8: "DocumentosPrueba/dni_tenesse.png",
+        9: "DocumentosPrueba/dni_uruguay.jpg",
+        10: "DocumentosPrueba/dni_usa.jpg"
+    }
+
+
+    print(rutas[num])
+    return rutas[num]
+
+
+
+
+# **
+# Lee la imagen y la convierte a escala de grises y luego la hace binaria
+# **
+def preprocess_image(image_path):
+    # Lee la imagen y la convierte a escala de grises
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    # Aplica un filtro GaussianBlur ligero para reducir el ruido
+    blurred = cv2.GaussianBlur(gray, (3, 3), 0)
+
+
+    # Aplica binarización Otsu para mejorar el contraste del texto
+    _, thresh_img = cv2.threshold(blurred, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+
+    # Retorna la imagen procesada (sin invertir para mantener texto oscuro y fondo claro)
+    return thresh_img
+
+
+# **
+# Extrae el texto de la imagen y crea una copia temporal de la imagen original
+# **
+def extract_text_from_image(image_path):
+    #se crea una imagen temporal copia de la imagen original pero procesada anteriormente y se extrae el texto
+   
+    processed_img= preprocess_image(image_path)
+
+
+    # data_image= pytesseract.image_to_data(processed_img, output_type=Output.DICT)
+   
+    # image_boxes(data_image, processed_img, image_path)
+   
+    temp_img_path = "dni_temporal.jpg"
+    cv2.imwrite(temp_img_path, processed_img)
+
+
+    text = pytesseract.image_to_string(Image.open(temp_img_path), config=custom_config)
+
+
+    print("*******************")
+    print(text)
+    print("*******************")
+
+
+    return text
+   
+
+
+# **
+# Coloca cajas en el texto que detecta pytesseract dentro de la imagen
+# **
+def image_boxes(data_image, processed_img, image_path):
+    n_boxes = len(data_image['text'])
+    imgColor = cv2.imread(image_path)
+
+
+
+
+    for i in range(n_boxes):
+        if int(data_image['conf'][i]) > 50 :
+            (x, y, w, h) = (data_image['left'][i], data_image['top'][i],data_image['width'][i], data_image['height'][i])
+            img = cv2.rectangle(processed_img, (x, y),(x + w, y + h),(255,0,0), 2)
+            imgColor = cv2.rectangle(processed_img, (x, y),(x + w, y + h),(255,0,0), 2)
+
+
+   
+    cv2.imwrite('ImagenRectangulos.jpg',img)
+    cv2.imwrite('ImagenRectangulosColor.jpg',imgColor )
+
+
+
+
+# **
+# Procesa el texto obtenido y lo convierte a JSON dependiendo del pais
+# **
+def process_image_text(text):
+
+
+    global type
+    global tipo_documento
+
+
+    #USA es el unico pais con formato distinto entonces se procesa aparte
+    usa_keywords=["USA","United States Of America","United States of America"]
+   
+    if any(keyword in text for keyword in usa_keywords):
+        tipo_documento = "USA"
+    elif "DNI" in text or "Documento Nacional de Identidad" in text:
+        tipo_documento = "DNI"
+    elif "Pasaporte" in text or "Passport" in text:
+        tipo_documento = "Pasaporte"
+    else:
+        tipo_documento = "Otro"
+   
+   
+    keywords=["Nombre","Nombres","Apellido","Apellidos","ID","Documento","RUN","RUT", "Nacimiento", "Fecha de Nacimiento"]
+    #futuro JSON donde se guardarán los datos
+   
+    data={}
+    lines = text.split('\n')
+   
+    data["TipoDocumento"] = tipo_documento
+
+
+    if tipo_documento == "USA":
+            for i, line in enumerate(lines):
+                if "Name" in line:
+                    # Las primeras dos líneas después del título son el nombre
+                    data["Nombre"] = " ".join(lines[i + 1:i + 3]).strip()
+                elif "Address" in line:
+                    # Las siguientes tres líneas son la dirección
+                    data["Direccion"] = " ".join(lines[i + 1:i + 4]).strip()
+                elif "License" in line:
+                    # La siguiente línea es el número de licencia
+                    data["NumeroLicencia"] = lines[i + 1].strip()
+                elif "DOB" in line or "Date of Birth" in line:
+                    # La siguiente línea es la fecha de nacimiento
+                    data["FechaNacimiento"] = lines[i + 1].strip()
+
+
+    else:
+        keywords = ["Nombre", "Nombres", "Apellido", "Apellidos", "ID", "Documento", "RUN", "RUT", "Nacimiento", "Fecha de Nacimiento"]
+        for i, line in enumerate(lines):
+            for keyword in keywords:
+                if keyword in line:
+                    if i + 1 < len(lines):
+                        # Extraer la línea siguiente al título como el valor
+                        data[keyword] = lines[i + 1].strip()
+
+
+
+
+    return data
+
+
+
+
+def dni_to_json(image_path):
+    text = extract_text_from_image(image_path)
+    dni_data= process_image_text(text)
+
+
+    dni_json = json.dumps(dni_data, indent=4, ensure_ascii=False)
+
+
+    return dni_json
+
+
+image_path = random_image()
+dni_json = dni_to_json(image_path)
+print(dni_json)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
---
+++ ocr_id_recognition/PreviousTests/Test.py
@@ -0,0 +1,160 @@
+import cv2
+import pytesseract
+import json
+from PIL import Image
+
+
+#El codigo este intenta extraer los datos de un documento
+
+
+# Configurar la ruta del ejecutable de Tesseract
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+# Cargar y preprocesar la imagen del DNI
+def preprocess_image(image_path):
+    img = cv2.imread(image_path)
+   
+    # Convertir a escala de grises
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+
+    # Aplicar un filtro de suavizado para reducir el ruido
+    gray = cv2.medianBlur(gray, 3)
+
+
+    # Opción de aplicar un umbral para mejorar el contraste
+    # ret, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
+
+    #Deteccion de bordes para resaltar areas con alto contraste
+    edges = cv2.Canny(gray, 50, 150)
+    #Contornos de la imagen del dni
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    #Dimensiones de la imagen
+    img_height, img_width = img.shape[:2]
+
+
+
+
+    for contour in contours:
+          # Obtiene el rectángulo delimitador de cada contorno
+        x, y, w, h = cv2.boundingRect(contour)
+
+
+        # Calcula el área del contorno y su proporción respecto al área total de la imagen
+        contour_area = w * h
+        img_area = img_width * img_height
+
+
+        # Si el área del contorno es significativa y tiene una forma rectangular (como una foto)
+        #if contour_area > 0.05 * img_area and 0.8 < w/h < 1.2:  # Evitar hardcodear valores fijos
+            # Dibujar un rectángulo blanco sobre la región de la imagen (para "eliminarla")
+        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 255, 255), -1)
+
+
+    return img
+
+
+# Extraer texto usando Tesseract
+def extract_text_from_image(image_path):
+    processed_img = preprocess_image(image_path)
+    # Guardar la imagen preprocesada como archivo temporal
+    temp_image_path = "dni_test_2.jpg"
+    cv2.imwrite(temp_image_path, processed_img)
+   
+    # Configurar Tesseract (psm 6 para un bloque de texto uniforme)
+    custom_config = r'--oem 3 --psm 6'
+   
+    # Extraer texto de la imagen preprocesada
+    text = pytesseract.image_to_string(Image.open(temp_image_path), config=custom_config)
+   
+    return text
+
+
+# Analizar el texto y convertirlo en un diccionario (JSON)
+def parse_dni_text(text):
+    data = {}
+
+
+
+
+    #dividimos el texto en lineas
+    lines = text.split('\n')
+    # Recorrer todas las líneas del texto
+    for i in range(len(lines)):
+        line = lines[i].strip()  # Limpiar espacios en blanco alrededor
+        keywords=["Nombre", "Apellido", "Documento", "Fecha De Nacimiento", "Nacionalidad", "Sexo"]
+
+
+        for keyword in keywords:
+            if(keyword in line):
+                if(keyword == "Sexo" or keyword == "Nacionalidad"):
+                    #si el titulo está en la linea, verificamos que haya una linea debajo
+                    if i + 1 < len(lines):
+                        #Extraemos de donde empieza el título
+                        start_pos = line.find(keyword)
+                        print(start_pos)
+                        #Extraemos la longitud del título
+                        keyword_length=len(keyword)
+                        #extraemos la proxima linea y sacamos los espacios al inicio y al costado
+                        next_line = lines[i + 1].strip()
+                        #Ahora de la linea sacamos solo lo que está debajo de las posiciones del titulo
+                        extracted_value = next_line[start_pos:start_pos + keyword_length].strip()
+                        data[keyword] = extracted_value
+                else:
+                    if i + 1 < len(lines):
+                        data[keyword] = lines[i + 1].strip()
+        """
+        # Verificamos si la línea contiene una palabra clave como "Nombre", "Apellido", etc.
+        if "Nombre" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["Nombre"] = lines[i + 1].strip()
+        elif "Apellido" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["Apellido"] = lines[i + 1].strip()
+        elif "Sexo" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["Sexo"] = lines[i + 1].strip()
+        elif "Nacionalidad" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["Nacionalidad"] = lines[i + 1].strip()
+        elif "Fecha de Nacimiento" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["FechaDeNacimiento"] = lines[i + 1].strip()
+        elif "Documento" in line :
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["DNI"] = lines[i + 1].strip()
+        elif "Fecha de Nacimiento" in line:
+            # Suponemos que el valor está en la siguiente línea
+            if i + 1 < len(lines):
+                data["Fecha_Nacimiento"] = lines[i + 1].strip()
+               
+    """
+    return data
+
+
+# Convertir los datos a JSON
+def dni_to_json(image_path):
+    text = extract_text_from_image(image_path)
+    dni_data = parse_dni_text(text)
+   
+    # Convertir el diccionario a JSON
+    dni_json = json.dumps(dni_data, indent=4, ensure_ascii=False)
+   
+    return dni_json
+
+
+# Ejemplo de uso
+image_path = "dni_test.jpg"  # Ruta de tu imagen de DNI
+dni_json = dni_to_json(image_path)
+print(dni_json)
+
+
+
