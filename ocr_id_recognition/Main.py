3fb70963b694d225efc3c7cd72dc82953c564306
Daniela Illanes <illanesdaniela28@gmail.com>
2024-10-17 12:26:58 -0300

    arreglos y ultimos detalles

--- ocr_id_recognition/Main.py
+++ ocr_id_recognition/Main.py
@@ -4,10 +4,11 @@ import json
 from PIL import Image
 import re
 import random
-from pytesseract import Output
 import numpy as np
 import pytesseract.pytesseract
 import ollama
+from datetime import datetime
+from dateutil import parser
 
 
 #configuracion de la orientacion de la pagina y el modelo de ocr
@@ -35,8 +36,8 @@ def random_image():
     }
 
 
-    print(rutas[num])
-    return rutas[num]
+    # print(rutas[num])
+    return rutas[11]
 
 
 
@@ -88,29 +89,6 @@ def extract_text_from_image(image_path):
 
 
     return text
-   
-
-
-# **
-# Coloca cajas en el texto que detecta pytesseract dentro de la imagen
-# **
-def image_boxes(data_image, processed_img, image_path):
-    n_boxes = len(data_image['text'])
-    imgColor = cv2.imread(image_path)
-
-
-
-
-    for i in range(n_boxes):
-        if int(data_image['conf'][i]) > 50 :
-            (x, y, w, h) = (data_image['left'][i], data_image['top'][i],data_image['width'][i], data_image['height'][i])
-            img = cv2.rectangle(processed_img, (x, y),(x + w, y + h),(255,0,0), 2)
-            imgColor = cv2.rectangle(processed_img, (x, y),(x + w, y + h),(255,0,0), 2)
-
-
-   
-    cv2.imwrite('ImagenRectangulos.jpg',img)
-    cv2.imwrite('ImagenRectangulosColor.jpg',imgColor )
 
 
 def analyze_text_ollama(text):
@@ -118,13 +96,15 @@ def analyze_text_ollama(text):
     model="llama2"
 
     prompt = (
-    "Extract the following information from the provided text, making sure that makes sense with the corresponding title" 
-    "and ignore single letters or rare non-letter characters (as long as they are not on the date): "
+    "Please extract the following information from the provided text while ensuring it corresponds accurately to the titles: "
     "Nombre, Apellido, FechaDeNacimiento, TipoDocumento, and Documento. "
-    "Please return the data in a JSON format with the exact field names specified before. "
+    "Transform the date of birth into a string format, such as '10 May 1998', using the month name in English. "
+    "Also, ignore any single letters or unusual non-letter characters. "
+    "Return the extracted data in JSON format with the exact field names specified above. "
     "Only provide the JSON response without any additional text. Here is the text:\n"
     f"{text}"
-)
+    )
+
 
     response = ollama.chat(model=model, messages=[
         {
@@ -147,9 +127,67 @@ def extract_json(text):
     else:
         return None  # Retornar None si no se encuentra un JSON
 
+def analyze_id_type(id_type):
+    id_type = id_type.lower()  # Convertimos todo el texto a minúsculas para facilitar comparaciones
+    
+    if "documento nacional de identidad" in id_type or "dni" in id_type or re.search(r'documento', id_type):
+        return "DNI"
+    
+    elif "cni" in id_type or "carte nationale d'identité" in id_type or "carte nationale" in id_type or re.search(r'cédula', id_type):
+        return "CNI"
+    
+    elif "ci" in id_type or "cédula de identidad" in id_type or re.search(r'cedula', id_type):
+        return "CI"
+    
+    elif "cpf" in id_type or "cadastro de pessoas fisicas" in id_type or "cadastro de pessoas físicas" in id_type:
+        return "CPF"
+    
+    elif "pasaporte" in id_type or "passport" in id_type:
+        return "PASAPORTE"
+    
+    else:
+        return "OTRO"
+    
+def analyze_dob(dob):
+    try:
+        # Analiza la fecha en varios formatos
+        fecha = parser.parse(dob)
+    except (ValueError, TypeError):
+        # Si ocurre un error, devolver 31 de diciembre de 1969
+        print("Error al analizar la fecha, devolviendo fecha predeterminada.")
+        fecha = datetime(1969, 12, 31)
+    
+    # Retorna la fecha en formato dd/mm/yyyy
+    return fecha.strftime("%d/%m/%Y")
+
+def final_json(extracted_data):
+    if(extracted_data):
+        new_json = {}
+
+        if "Nombre" in extracted_data:
+            new_json["Nombre"] = extracted_data["Nombre"]
+        if "Apellido" in extracted_data:
+            new_json["Apellido"] = extracted_data["Apellido"]
+        if "Documento" in extracted_data:
+            new_json["Documento"] = extracted_data["Documento"]
+        if "FechaDeNacimiento" in extracted_data or "fecha de nacimiento" in extracted_data.lower():
+            fechaDeNacimiento = analyze_dob(extracted_data["FechaDeNacimiento"])
+            new_json["FechaDeNacimiento"] = fechaDeNacimiento
+        if "TipoDocumento" in extracted_data or "tipo documento" in extracted_data.lower():
+            tipoDocumento = analyze_id_type(extracted_data["TipoDocumento"])
+            new_json["TipoDocumento"] = tipoDocumento
+
+        new_json_str = json.dumps(new_json, indent=4, ensure_ascii=False)
+        return new_json_str
+    else:
+        return "No se encontró un JSON válido."
 
 image_path = random_image()
 text = extract_text_from_image(image_path)
 ollama_text= analyze_text_ollama(text)
-final_json = extract_json(ollama_text)
+
+ollama_json = extract_json(ollama_text)
+print(ollama_json)
+
+final_json = final_json(ollama_json)
 print(final_json)
